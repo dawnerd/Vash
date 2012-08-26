8acc18d554ce0495e9ec0a16f4353c742508e66c
Troy Whiteley <troy@somanyscientists.com>
2012-08-26 02:01:27 -0700

    Add start of JS framework
    Added main layout view controller and a master app.js

---
+++ public/ui/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Vash - Dashboard for your media
+ *
+ * https://github.com/dawnerd/Vash
+ */
+
+(function(){
+  var layout = new this.vash.layoutViewController({el: $('body')});
+}());
---
+++ public/ui/js/controllers/layoutViewController.js
@@ -0,0 +1,22 @@
+/**
+ * Vash - Dashboard for your media
+ *
+ * https://github.com/dawnerd/Vash
+ *
+ * This controller manages the header, and footer.
+ */
+
+(function(){
+  this.vash.layoutViewController = Fidel.ViewController.extend({
+    initialize: function() {
+      this.header();
+      this.footer();
+    },
+    header: function() {
+
+    },
+    footer: function() {
+
+    }
+  });
+}());
